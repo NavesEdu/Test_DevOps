da8ccbcbc60abf2d0eaa5e474a55e0f1dce1ea34
Eduardo Naves <eduardonaves41@gmail.com>
2025-09-28 00:27:53 -0300

    Fixing JCasC and Agents

--- jenkins-setup/agent.sh
+++ jenkins-setup/agent.sh
@@ -7,7 +7,6 @@ sleep 15
 
 echo "Registering '${JENKINS_AGENT_NAME}' with the controller..."
 
-# Retrieve the agent secret from its metadata endpoint.
 AGENT_ENDPOINT="${JENKINS_URL}/computer/${JENKINS_AGENT_NAME}/jenkins-agent.jnlp"
 AGENT_METADATA="$(curl -s -X GET -u "${JENKINS_AGENT_USERNAME}:${JENKINS_AGENT_PASSWORD}" $AGENT_ENDPOINT)"
 AGENT_SECRET="$(echo $AGENT_METADATA | sed "s/.*<application-desc><argument>\([a-z0-9]*\).*/\1\n/")"
